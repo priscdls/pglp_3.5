b74a69e43f334ce2ce6fc381c81ae52a0f8fc047
Priscille Daoulas <priscilledaoulas@gmail.com>
2020-03-06 23:51:40 +0100

    Checkstyle et pom

---
+++ pglp_3_5/src/main/java/Priscille_DAOULAS/pglp_3_5/App.java
@@ -0,0 +1,18 @@
+package Priscille_DAOULAS.pglp_3_5;
+/**
+ * Classe App.
+ */
+public final class App {
+	/**
+	 * Constructeur.
+	 */
+	private App() {
+	}
+	/**
+	 * Début du programme.
+	 * @param args Les arguments donnés au démarrage de l'application
+	 */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
